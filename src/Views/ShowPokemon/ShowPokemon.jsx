0738dc1fd28fcdc1d66d23424d469ca796f89a02
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-11-12 19:47:41 +0100

    working on show pokemons

--- src/Views/ShowPokemon/ShowPokemon.jsx
+++ src/Views/ShowPokemon/ShowPokemon.jsx
@@ -1,6 +1,54 @@
 import './ShowPokemon.css';
+import { pokemonDataCheck } from "../pokemonSlice";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { bringPokemonData } from '../../services/apiCalls';
 
 export const ShowPokemon = () => {
+    const reduxPokemonData = useSelector(pokemonDataCheck);
+    console.log("soy reduxPokemonData", reduxPokemonData)
+    const [pokemonUrl, setPokemonUrl] = useState();
+    const [pokemonData, setPokemonData] = useState({
+        nombre:  "",
+        habilidades:  [],
+        imagen:  "",
+        hpBase:  "",
+        AttackBase:  "",
+        defenseBase:  "",
+        specialAttackBase:  "",
+        specialDefenseBase: "",
+        Speed:  ""
+      });
+
+    const [habilidades] = useState([]);
+
+    const seterHandler = (value, stat) => {
+        setPokemonData((prevState) => ({
+        ...prevState,
+        [stat]: value,
+      }));
+    };
+
+    useEffect(() => {
+        setPokemonUrl(reduxPokemonData.pokemonData.url);
+      });
+     console.log("soy pokemonUrl", pokemonUrl);
+
+    useEffect(() => {
+     bringPokemonData(pokemonUrl)
+      .then(
+         resultado => {
+            for (let i=0; i < resultado.data.abilities.length; i++) {
+               habilidades.push(resultado.data.abilities[i].ability.name)
+               console.log("soy habilidades", habilidades)
+            }
+            //  console.log("soy pokemonData", pokemonData)
+            //  console.log("soy resultado", resultado)
+         }
+      )
+      .catch(error => console.log(error));
+    });
+
     return (
         <>showpokemon</>
     )
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const bringPokemons = async () => {
     return await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
 }
+
+export const bringPokemonData = async (url) => {
+    return await axios.get(`${url}`);
+}
